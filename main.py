2b8d344dac962a1f76aaad4f81dcf9b6ac658359
Kacper Kowalski <xperski@pm.me>
2025-03-27 05:19:30 +0100

    auto-commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uvicorn
 from os import getenv
 from bson import ObjectId
 from fastapi import FastAPI, Request, HTTPException, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
@@ -14,6 +14,7 @@ uri = f"mongodb+srv://persky:{DB_PASSWORD}@cluster0.jevvu.mongodb.net/?appName=C
 client = MongoClient(uri)
 db = client["forum_db"]
 collection_posts = db["posts"]
+collection_comments = db["comments"]
 collection_users = db["users"]
 
 app = FastAPI()
@@ -30,22 +31,62 @@ class Post(BaseModel):
     created_at: str
 
 
+class Comment(BaseModel):
+    id: str
+    post_id: str
+    content: str
+    created_at: str
+
+
 class User(BaseModel):
     user_id: str
     username: str
     password: str
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     posts = list(collection_posts.find().sort("created_at", -1))
+    for post in posts:
+        post["_id"] = str(post["_id"])
     return templates.TemplateResponse("index.html", {"request": request, "posts": posts})
+
+
+@app.get("/post/{post_id}", response_class=HTMLResponse)
+def get_post(request: Request, post_id: str):
+    post = collection_posts.find_one({"_id": ObjectId(post_id)})
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    post["_id"] = str(post["_id"])
+    comments = list(collection_comments.find({"post_id": post_id}).sort("created_at", -1))
+    for comment in comments:
+        comment["_id"] = str(comment["_id"])
+        comment["created_at"] = comment["created_at"].strftime("%H:%M")
+    return templates.TemplateResponse("post.html", {"request": request, "post": post, "comments": comments})
+
+
+@app.post("/post/{post_id}/comment", response_class=HTMLResponse)
+def add_comment(request: Request, post_id: str, content: str = Form(...)):
+    new_comment = {
+        "_id": ObjectId(),
+        "post_id": post_id,
+        "content": content,
+        "created_at": datetime.now(),
+    }
+    collection_comments.insert_one(new_comment)
+    return RedirectResponse(url=f"/post/{post_id}", status_code=303)
+
+
+@app.get("/create", response_class=HTMLResponse)
+def create_post_form(request: Request):
+    return templates.TemplateResponse("create.html", {"request": request})
 
 
 @app.post("/create", response_class=HTMLResponse)
 def create_post(request: Request, title: str = Form(...), content: str = Form(...)):
     new_post = {
-        "id": str(ObjectId()),
+        "_id": ObjectId(),
         "title": title,
         "content": content,
         "author_id": "Anonymous",
@@ -55,35 +96,25 @@ def create_post(request: Request, title: str = Form(...), content: str = Form(..
     return templates.TemplateResponse("create_success.html", {"request": request})
 
 
-@app.post("/create", response_class=HTMLResponse)
-def create_post(request: Request, title: str = Form(...), content: str = Form(...)):
-    new_post = {
-        "id": str(ObjectId()),
-        "title": title,
-        "content": content,
-        "author_id": "Anonymous"
-    }
-    collection_posts.insert_one(new_post)
-    return templates.TemplateResponse("create_success.html", {"request": request})
-
-
 @app.delete("/delete/{post_id}")
-def delete_post(request: Request, post_id: str):
-    result = collection_posts.delete_one({"id": post_id})
+def delete_post(post_id: str):
+    result = collection_posts.delete_one({"_id": ObjectId(post_id)})
     if result.deleted_count == 1:
         return {"result": "Post deleted"}
     return {"result": "Post can't be deleted"}
 
 
-@app.get("/get-post/{post_id}")
+@app.get("/get-post/{post_id}", response_class=HTMLResponse)
 def get_post(request: Request, post_id: str):
-    post = collection_posts.find_one({"id": post_id})
+    post = collection_posts.find_one({"_id": ObjectId(post_id)})
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
+
+    post["_id"] = str(post["_id"])
     return post
 
 
-@app.get("/get-user/{user_id}")
+@app.get("/get-user/{user_id}", response_class=HTMLResponse)
 def get_user(request: Request, user_id: str):
     user = collection_users.find_one({"user_id": user_id})
     if not user:
